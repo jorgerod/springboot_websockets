e817b016871e46c44db775f2ff74581838e4a496
Jorge Rodriguez <jorgerod84@gmail.com>
2015-05-11 21:11:56 +0200

    Test controller

---
+++ src/main/java/websocket/controllers/HelloWorldController.java
@@ -0,0 +1,17 @@
+package websocket.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class HelloWorldController {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String get (@RequestParam String name) {
+        return "Hola" + name; 
+    }
+    
+}
