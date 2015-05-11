9030d6bb0ae975f2d0bc93eb6efd36b02d7d1683
Jorge Rodriguez <jorgerod84@gmail.com>
2015-05-11 23:27:55 +0200

    Recibe una petición get y reenvia un texto.

--- src/main/java/websocket/controllers/ChatController.java
+++ src/main/java/websocket/controllers/ChatController.java
@@ -1,17 +1,31 @@
 package websocket.controllers;
 
+import java.util.GregorianCalendar;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 @RestController
-@RequestMapping("/greeting")
+@RequestMapping("/chat")
 public class ChatController {
 
     @RequestMapping(method=RequestMethod.GET)
-    public String get (@RequestParam String name) {
-        return "Hola, " + name; 
+    public String get (@RequestParam String text) {
+        return new Gson().toJson(new Message(text)); 
+    }
+    
+    class Message {
+        private String text = null;
+        private Long time = null;
+        
+        public Message (String text) {
+            this.text = "Hola " + text;
+            this.time = GregorianCalendar.getInstance().getTimeInMillis();
+        }
     }
     
 }
