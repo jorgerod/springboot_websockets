5f267df329dda633229c7ebebc7131e57164a446
Jorge Rodriguez <jorgerod84@gmail.com>
2015-05-11 22:09:55 +0200

    Rename chatController

--- src/main/java/websocket/controllers/ChatController.java
+++ src/main/java/websocket/controllers/ChatController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
-public class HelloWorldController {
+public class ChatController {
 
     @RequestMapping(method=RequestMethod.GET)
     public String get (@RequestParam String name) {
-        return "Hola,, " + name; 
+        return "Hola, " + name; 
     }
     
 }
