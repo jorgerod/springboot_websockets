80029d24ed70b67e8553215655c982b4186f7a4c
Jorge Rodriguez <jorgerod84@gmail.com>
2015-05-11 21:48:48 +0200

    zzz

--- src/main/java/websocket/SpringbootWebsocketApplication.java
+++ src/main/java/websocket/SpringbootWebsocketApplication.java
@@ -8,6 +8,5 @@ public class SpringbootWebsocketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebsocketApplication.class, args);
-        System.out.println("empezando");
     }
 }
